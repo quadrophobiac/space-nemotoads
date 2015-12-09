29c9adb56337891c38e1ed55527ee35c4494956d
quadrophobiac <stephen.j.fortune@gmail.com>
2015-12-09 19:01:18 +0000

    posterity log of failure to use mocha on async trimming fat of this because testing is waste of time RN

--- caltech-poll.js
+++ caltech-poll.js
@@ -13,27 +13,82 @@ function CaltechPoll(){}
 CaltechPoll.prototype.log = function(){
     return "word";
 }
+CaltechPoll.prototype.reqObj = function(){
+    CaltechPoll.prototype.store = restclient.request('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_rade,pl_bmasse,pl_disc&where=pl_rade%3C2.0&format=csv',
+        {
+            accept: '*/*',
+            'user-agent': 'Restler for node.js',
+            host: 'exoplanetarchive.ipac.caltech.edu',
+            'accept-encoding': 'gzip, deflate',
+            'content-length': 0
+        });
+}
 
-restclient.get('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_rade,pl_bmasse,pl_disc&where=pl_rade%3C2.0&format=csv')
-    .on('success', function(result, response){
-        console.log(typeof result);
-        var arr = CSV.parse(result);
-        console.log(arr);
-
-        fs.writeFile(__dirname+'/exo.csv', result, 'utf8');
+CaltechPoll.store.on('complete', function(){
+    var path = __dirname+'/exo.csv';
+})
 
 
-    })
-    .on('complete', function(data){
-        //console.log(JSON.stringify(data, null, 4));
-        // console.log(data[1]);
+CaltechPoll.prototype.saveCSV = function(path){
+    console.log(path === void 0);
+    //path === void 0 ? path : __dirname+'exo.csv'
+    //path === void 0 ? __dirname+'exo.csv' : path
+    if(path === void 0 ){
+        path = __dirname+'/exo.csv';
+    }
+    console.log("in csv function");
+    console.log(__dirname);
+    console.log(path);
+    restclient.get('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_rade,pl_bmasse,pl_disc&where=pl_rade%3C2.0&format=csv')
+    //restclient.request    ('http://.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_rade,pl_bmasse,pl_disc&where=pl_rade%3C2.0&format=csv')
+        .on('success', function(result, response){
+            //console.log(typeof result);
+            console.log(response.statusCode);
+            var arr = CSV.parse(result);
+            console.log(arr[0]);
 
-        // console.log(Object.getOwnPropertyNames(data));
-    })
-    .on('error', function(result,response){
-        console.log("error occurred: ");
-        console.log(response);
+            fs.writeFileSync(path, result, 'utf8');
+            CaltechPoll.savedFile = path;
+            return(response.statusCode);
 
-    });
+        })
+        .on('complete', function(result, response){
+            console.log(response);
+            console.log("completed call");
+            return true;
+            //console.log(JSON.stringify(data, null, 4));
+            // console.log(data[1]);
+
+            // console.log(Object.getOwnPropertyNames(data));
+        })
+        .on('error', function(result,response){
+            console.log("err"+result.errno+" the type "+Object.getOwnPropertyNames(result));
+            console.log(result.stack+","+result.message+","+result.code+""+result.errno+""+result.syscall+""+result.hostname+""+result.host+""+result.port)
+
+        });
+
+}
+
+//restclient.get('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_rade,pl_bmasse,pl_disc&where=pl_rade%3C2.0&format=csv')
+//    .on('success', function(result, response){
+//        console.log(typeof result);
+//        var arr = CSV.parse(result);
+//        console.log(arr);
+//
+//        fs.writeFile(__dirname+'/exo.csv', result, 'utf8');
+//
+//
+//    })
+//    .on('complete', function(data){
+//        //console.log(JSON.stringify(data, null, 4));
+//        // console.log(data[1]);
+//
+//        // console.log(Object.getOwnPropertyNames(data));
+//    })
+//    .on('error', function(result,response){
+//        console.log("error occurred: ");
+//        console.log(response);
+//
+//    });
 
 module.exports = CaltechPoll;
