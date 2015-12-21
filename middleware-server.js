f0fa324b24a03c64e02332c55082d8fbba6c3840
quadrophobiac <stephen.j.fortune@gmail.com>
2015-12-21 09:50:48 +0000

    pull data from filesystem on server start

--- middleware-server.js
+++ middleware-server.js
@@ -1,5 +1,7 @@
 // via https://codeforgeek.com/2015/07/unit-testing-nodejs-application-using-mocha/
 
+var fs = require('fs');
+
 var express = require('express');
 var router = express.Router();
 var five = require("johnny-five");
@@ -8,6 +10,12 @@ var app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+var lineCount;
+
+var planets = fs.readFileSync(__dirname+"/fixtures/earthSized.json",'utf8');
+var data = JSON.parse(planets);
+var top3 = data.sort(function(a, b) { return a.Variable1 < b.Variable1 ? 1 : -1; }).slice(0, 3);
+console.log(Object.getOwnPropertyNames(top3));
 
 //Arduino board connection
 var led;
@@ -17,8 +25,6 @@ board.on("ready", function() {
     led = new five.Led(13);
     led.off();
 });
-
-
 
 router.get('/',function(req,res){
     res.json({"error" : false, "message" : "Hello !"});
@@ -30,6 +36,7 @@ router.post('/add',function(req,res){
     res.json({"error" : false, "message" : "success", "data" : req.body.num1 + req.body.num2});
     led.on();
 });
+
 
 app.use('/',router);
 
