27ebbbaa09b724e4feb348308c7b6e6906be16c9
quadrophobiac <stephen.j.fortune@gmail.com>
2015-12-21 09:34:01 +0000

    including get spec NB - the middleware-server.js has to be initialised before the tests pass

--- test/arduino-server-spec.js
+++ test/arduino-server-spec.js
@@ -15,6 +15,22 @@ var server = supertest.agent("http://localhost:3000");
 
 describe('socket server', function(){
 
+    it("should return home page",function(done){
+
+        // calling home page api
+        server
+            .get("/")
+            .expect("Content-type",/json/)
+            .expect(200) // THis is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+                done();
+            });
+    });
+
     it("should add two number",function(done){
 
         //calling ADD api
