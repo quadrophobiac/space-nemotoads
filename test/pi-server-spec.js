332182c2a81af0202bfb5931153e7f94ad486b2d
quadrophobiac <stephen.j.fortune@gmail.com>
2015-12-27 22:12:32 +0000

    proper TDD, write. fail. refactor

---
+++ test/pi-server-spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+describe.skip('server', function(){
+
+    afterEach('log files should have changed', function(){
+
+    });
+
+    it('should start and listen on port ENV:????', function(){
+
+        // assert - code that verifies actually working
+        // assert - changes have been made to log file
+
+    });
+
+    it('serves static page on $HOST:$PORT/index.html', function(){
+
+    });
+
+    it('catchs erroneous access without crashing', function(){
+
+    });
+
+});
+
+describe.skip('boot sequence', function(){
+
+    afterEach('log files should have changed', function(){
+
+    });
+
+
+    it('reads from local exo db file', function(){
+
+    });
+
+    it('starts the arduino', function(){
+
+    });
+
+    it('passes parameters to the arduino', function(){
+
+        // read current state of initialised motors
+        // pass params
+        //
+
+    });
+
+
+});
