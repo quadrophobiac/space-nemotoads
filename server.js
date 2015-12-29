451b7da24cfaaefe3b41f5502696ea57c5eb7675
quadrophobiac <stephen.j.fortune@gmail.com>
2015-12-29 21:56:01 +0000

    manually logging server changes

--- server.js
+++ server.js
@@ -4,10 +4,21 @@
 var express = require('express');
 var app = express();
 var assignedPort = 3000;
+var fs = require('fs');
+var logfile = require('./logger.js');
+
 app.get('/', function(req,res){
+    fs.appendFileSync(__dirname+"/logs/server.log", "GET REQ RECEIVED\n", 'utf8');
     res.status(200).send('ok\n');
 });
+
+app.get('/index.html', function(req,res){
+    res.status(200).send('ok\n');
+});
+
 var server = app.listen(assignedPort, function(){
+
+    fs.appendFileSync(__dirname+"/logs/server.log", "started\n", 'utf8');
     var port = server.address().port;
     console.log('server listening at port %s', port);
 });
