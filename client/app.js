38191cbdc9c225122d801b7c366b17f615aa8cb2
quadrophobiac <stephen.j.fortune@gmail.com>
2016-01-28 16:40:12 -0800

    add PlanetsModel to backbone app

--- client/app.js
+++ client/app.js
@@ -1,3 +1,21 @@
 /**
  * Created by stephenfortune on 28/01/2016.
  */
+(function() {
+    // some code here
+    var PlanetsModel = Backbone.Model.extend({
+
+        defaults: function(){
+            return{
+                pl_name: "",
+                pl_rade: "",
+                pl_bmasse: "",
+                pl_disc: "",
+                rowupdate: ""
+            }
+        }
+    });
+
+
+
+})();
