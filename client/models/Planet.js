bdd0a84ed1d3dea67e87346b15ecc4631680a2c8
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-06 15:24:17 +0000

    refactor to use calculation this calculation should be in use on server side once pushed to production, including it here for dev purposes

--- client/models/Planet.js
+++ client/models/Planet.js
@@ -2,15 +2,24 @@
  * Created by stephenfortune on 03/02/2016.
  */
 var Backbone = require('backbone');
+var gravity = require('./../../lib/surfaceG');
+
+console.log(gravity.surfaceGravity(1,1,1));
+
 var Planet = Backbone.Model.extend({
     defaults: {
         pl_name: '',
         pl_rade: '',
         pl_bmasse: '',
         pl_disc: '',
-        rowupdate: ''
+        rowupdate: '',
+        gravity: '0'
+    },
+    initialize: function(){
+        this.set('gravity', gravity.surfaceGravity(this.get('pl_bmasse'),this.get('pl_rade')));
     }
 });
+
 
 module.exports = function (opt) {
     return new Planet(opt);
