c69c7f1580066962e9547adb043bf3a789a8648d
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-08 20:10:57 +0000

    update to app that is browserified jquery resolution = TODO

--- client/app.js
+++ client/app.js
@@ -1,2 +1,5 @@
+var jQuery = require('jquery');
+var bootstrap = require('bootstrap');
+//TODO - browserify and bootstrap are not playing nicely together
 var PlanetsView = require('./views/PlanetsView');
 new PlanetsView();
