0c4d85dbee6fe4f12c727111f79b0fd52f75ba2c
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-08 16:10:52 +0000

    amended URL routing Backbone.Sync does the heavy lifting to find routes correctly

--- client/collections/PlanetsCollection.js
+++ client/collections/PlanetsCollection.js
@@ -2,15 +2,15 @@
  * Created by stephenfortune on 03/02/2016.
  */
 var Backbone = require('backbone');
+var _ = require('underscore');
 
 var Planet = require('./../models/Planet');
 
 var PlanetsCollection = Backbone.Collection.extend({
 
     model: Planet,
-    url: 'http://localhost:3000/planets',
-    comparator: 'rowupdate'
-
+    comparator: 'rowupdate',
+    url: '/planets'
 });
 
 module.exports = function () {
