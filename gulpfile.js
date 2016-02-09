c2431457216993812c992bdee16c71a1aedb8b53
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-09 01:06:30 +0000

    gulpfile for automatic loading nodemon watches forJSON changes, reloads app on change detect, browser window syncs to changes

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+var browserify 	= require('gulp-browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+			proxy: "http://localhost:5000",
+        files: ["client/data/*.*"],
+        browser: "google chrome",
+        port: 7000,
+	});
+});
+gulp.task('nodemon', function (cb) {
+
+	var started = false;
+
+	return nodemon({
+		script: 'server.js'
+		, ext: 'js html json'
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		// thanks @matthisk
+		if (!started) {
+			cb();
+			started = true;
+		}
+	});
+});
