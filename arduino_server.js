f2c2e96d9f3f159812653d0069dbc02386b0e6c0
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-11 16:51:25 +0000

    import modular johny-5 arduino as requirement

--- arduino_server.js
+++ arduino_server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var assignedPort = 3000;
 var app = express();
 var planetdata = require('./fixtures/earthSized.json');
+var arduino = require('./arduino_master');
+
 
 app.get('/start', function(req,res){
     res.status(200).send('ok\n');
     console.log('start motors');
+    arduino.startsteppers(180,20000,200);
 });
 
 app.get('/pause', function(req,res){
@@ -26,6 +29,7 @@ var server = app.listen(assignedPort, function(){
     //fs.appendFileSync(__dirname+"/logs/server.log", "started\n", 'utf8');
     var port = server.address().port;
     console.log('server listening at port %s', port);
+
     //logger.log('info', 'server started');
 
     //startarduino();
