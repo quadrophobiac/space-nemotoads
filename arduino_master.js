b8f8d28f209a91b5dc931d1ba0065f6ec49ce2be
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-11 10:55:05 +0000

    stepper library works with caveat pre cruft cleaning commit cleaning cruft from this code as stepper.js is not analagous to motor.js (less sophisticated, no event emitters) ergo wrapper will be necessitated

--- arduino_master.js
+++ arduino_master.js
@@ -17,7 +17,6 @@ board.on("ready", function() {
         }
     });
 
-
     var motorReporter = function(err, stamp){
         console.log("logging callback func");
 
@@ -27,16 +26,72 @@ board.on("ready", function() {
             console.log(this.id+", running = "+this.isOn+", current speed = "+this.currentSpeed);
         }
     }
-    console.log(Object.getOwnPropertyNames(motor));
+    //console.log(Object.getOwnPropertyNames(motor));
+    //
+    //motor.on("forward", motorReporter);
+    //motor.on("stop", motorReporter);
+    //motor.on("start", motorReporter);
 
-    motor.on("forward", motorReporter);
-    motor.on("stop", motorReporter);
-    motor.on("start", motorReporter);
+    //stepper1s
+
+    //Arduino syntax
+    //Accelstepper1 stepper11(1,10,9); // stepper1 ( , StepPin, DirectionPin )
+
+
+
+    var stepper1 = new five.Stepper({
+        type: five.Stepper.TYPE.DRIVER,
+        stepsPerRev: 200,
+        rpm: 180,
+        pins: {
+            step: 10,
+            dir: 9
+        }
+        // it is better to pass accel and decel as params to a function as they impact speed otherwise
+        //eg function stepper1.rpm(189).decel(1600).ccw().step(8000, function(){});
+    });
+    var stepper2 = new five.Stepper({
+        type: five.Stepper.TYPE.DRIVER,
+        stepsPerRev: 200,
+        rpm: 180,
+        pins: {
+            step: 2,
+            dir: 1
+        },
+        //speed: 0.75400, increasing this from 0 slows the motor
+        accel: 1600,
+        decel: 1600
+    });
+    console.log(Object.getOwnPropertyNames(stepper1));
+    var stepperReporter = function(err,stamp){
+        console.log(this);
+    }
+    //
+    stepper1.on("step", stepperReporter);
+    //console.log(Object.getOwnPropertyNames(stepper2));
+    //console.log(stepper2.speed());
+
+
+    //stepper1.rpm(180).ccw().accel(1600).decel(1600).step(2000, function() {
+    //
+    //    console.log("Done moving CCW");
+    //
+    //    // once first movement is done, make 10 revolutions clockwise at previously
+    //    //      defined speed, accel, and decel by passing an object into stepper1.step
+    //    stepper1.step({
+    //        steps: 2000,
+    //        direction: five.stepper1.DIRECTION.CW
+    //    }, function() {
+    //        console.log("Done moving CW");
+    //    });
+    //});
 
     // make the LED accessible from REPL command line
     this.repl.inject({
         led: led,
-        motor: motor
+        motor: motor,
+        stepper1: stepper1,
+        stepper2: stepper2
     });
 
 });
