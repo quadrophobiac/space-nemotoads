2a3ba4f20c03df78cce0383b0a5d404a04c4b6cc
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-13 19:03:42 +0100

    mended gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -8,72 +8,97 @@ var nodemon = require('gulp-nodemon');
 var source = require('vinyl-source-stream');
 var request = require('request');
 
-var CROSS_SERVER_REQUEST_DELAY = 500;
 
-gulp.task('default', ['browser-sync'], function () {
-	//setTimeout(function defaultpause() {ping();},500);
-}); // browsersync wont play nice with linux
+var BROWSER_SYNC_RELOAD_DELAY = 500;
 
-gulp.task('browser-sync', ['nodemon'], function() {
-	console.log('booting browser sync');
+gulp.task('default', ['browser-sync'], function () {});
 
+gulp.task('nodemon', function (cb) {
+	var called = false;
+	return nodemon({
+
+		script: 'server.js',
+
+		watch: ['fixtures/earthSized.json']
+	})
+		.on('start', function onStart() {
+			// ensure start only got called once
+			if (!called) { cb(); }
+			called = true;
+		})
+		.on('restart', function onRestart() {
+			// reload connected browsers after a slight delay
+			setTimeout(function reload() {
+				browserSync.reload({
+					stream: false
+				});
+			}, BROWSER_SYNC_RELOAD_DELAY);
+		});
+});
+
+gulp.task('browser-sync', ['nodemon'], function () {
+
+	// for more browser-sync config options: http://www.browsersync.io/docs/options/
 	browserSync({
 
 		// informs browser-sync to proxy our expressjs app which would run at the following location
-		proxy: "http://localhost:5000",
-		files: ["fixtures/*.json"],
+		proxy: 'http://localhost:5000',
+
 		// informs browser-sync to use the following port for the proxied app
 		// notice that the default port is 3000, which would clash with our expressjs
 		port: 7000,
-
+		files: ["fixtures/*.json"],
 		// open the proxied app in chrome
-		browser: "firefox"
-	});
-}); // browsersync wont play nice with linux
-
-gulp.task('nodemon', function (cb) {
-
-	var started = false;
-
-	return nodemon({
-		script: 'server.js'
-		, ext: 'js html'
-	}).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		console.log('nodemon has started');
-		if (!started) {
-			console.log("triggering callback from 'start only once'");
-			cb();
-			started = true;
-		}
-	}).on('restart', function () {
-		setTimeout(function reload() {
-			ping();
-			browserSync.reload({
-				stream: false
-			});
-		}, CROSS_SERVER_REQUEST_DELAY);
+		browser: process.platform === 'darwin' ? 'firefox' : 'chromium'
 	});
 });
 
-var ping = function(){
-	console.log("pinging");
-	request('http://localhost:5000/planets',reqcb);
-}
 
-var reqcb = function(error, response, body){
-	console.log("requesting");
-	if (!error && response.statusCode == 200) {
-		console.log("success");
-		if(response){console.log(response.statusCode);}
-		//if(body){console.log(body);}
-		//console.log(body); // Show the HTML for the Modulus homepage.
-	} else {
-		console.log("fail");
-		console.log(error.stack);
-		if(response){console.log(response.statusCode);}
-		//if(body){console.log(body);}
-		//console.log(body);
-	}
-}
+//var CROSS_SERVER_REQUEST_DELAY = 500;
+//
+//gulp.task('default', ['browser-sync'], function () {
+//	//setTimeout(function defaultpause() {ping();},500);
+//}); // browsersync wont play nice with linux
+//
+//gulp.task('browser-sync', ['nodemon'], function() {
+//	console.log('booting browser sync');
+//
+//	browserSync({
+//
+//		// informs browser-sync to proxy our expressjs app which would run at the following location
+//		proxy: "http://localhost:5000",
+//		files: ["fixtures/*.json"],
+//		// informs browser-sync to use the following port for the proxied app
+//		// notice that the default port is 3000, which would clash with our expressjs
+//		port: 7000,
+//
+//		// open the proxied app in chrome
+//		browser: "firefox"
+//	});
+//}); // browsersync wont play nice with linux
+//
+//gulp.task('nodemon', function (cb) {
+//
+//	var started = false;
+//
+//	return nodemon({
+//		script: 'server.js'
+//		, ext: 'js html'
+//	}).on('start', function () {
+//		// to avoid nodemon being started multiple times
+//		// thanks @matthisk
+//		console.log('nodemon has started');
+//		if (!started) {
+//			console.log("triggering callback from 'start only once'");
+//			cb();
+//			started = true;
+//		}
+//	}).on('restart', function () {
+//		setTimeout(function reload() {
+//			ping();
+//			browserSync.reload({
+//				stream: false
+//			});
+//		}, CROSS_SERVER_REQUEST_DELAY);
+//	});
+//});
