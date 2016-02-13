efa9fcb2464d33770c0cdbc2a8d0657f44b1cf0d
quadrophobiac <stephen.j.fortune@gmail.com>
2016-02-13 14:18:10 +0100

    refactor arduino server reset endpoint returns 503 when motors are still running returns 200 when motors halted, triggers automatic server close

--- arduino_server.js
+++ arduino_server.js
@@ -3,14 +3,18 @@ var assignedPort = 3000;
 var app = express();
 var planetdata = require('./fixtures/earthSized.json');
 var arduino = require('./arduino_master');
+var topthree = require('./lib/uniform');
+
+var gravtorpm = function(){
+    var planets = topthree.recent();
+}
 
 var rpmVals = {
     0: 180,
     1: 154,
     2: 115.8
 }
-//var rpmVals = {0: 180, 1: 154, 2: 115.8}
-//1.18 1.28 1.41 avg = 1.29
+
 
 var retrieveGravity = function(){
     // TODO function from lib that screen and arduino share
@@ -19,19 +23,23 @@ var retrieveGravity = function(){
 app.get('/start', function(req,res){
     res.status(200).send('ok\n');
     console.log('start motors');
-    arduino.startsteppers(rpmVals,20000);
+    arduino.startsteppers(rpmVals,8000);
 });
 
-app.get('/pause', function(req,res){
-    // pause server and motors
-    res.status(200).send('ok\n');
-    console.log('begin pausing');
-});
 
 app.get('/reset', function(req,res){
-    // pause server and motors
-    res.status(200).send('ok\n');
-    console.log('motors halt, reboot');
+
+    if(arduino.stepperStatus()){
+        console.log("condition IF ");
+        res.status(503).send('retry\n');
+    } else {
+        console.log("condition ELSE ");
+        res.status(200).send('ok\n');
+        server.close(function() {
+            console.log("Closed out remaining connections.");
+            process.exit()
+        });
+    }
 });
 
 var server = app.listen(assignedPort, function(){
@@ -40,10 +48,8 @@ var server = app.listen(assignedPort, function(){
     //fs.appendFileSync(__dirname+"/logs/server.log", "started\n", 'utf8');
     var port = server.address().port;
     console.log('server listening at port %s', port);
-
+    console.log("planet = "+JSON.stringify(topthree.recent(),null,4));
     //logger.log('info', 'server started');
-
-    //startarduino();
-
 });
+
 module.exports = server;
